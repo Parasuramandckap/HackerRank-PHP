f4806c1ac08508142f7c6f03f31de296fa85cc6c
Parasuramandckap <parasuaptadckap@gmail.com>
2023-05-25 16:08:49 +0530

    staircase problem solved

---
+++ staircase.php
@@ -0,0 +1,15 @@
+<?php
+function staircase($n) {
+    // Write your code here
+    for($i =1; $i <= $n; $i++){
+        echo str_repeat(".", $n - $i);
+        echo str_repeat("#", $i);
+       
+
+        // Starts printing on a new line
+         echo "\n";
+    }
+   
+
+}
+staircase(8);
